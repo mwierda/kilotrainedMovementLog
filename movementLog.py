3012863f69d0047cd6be7d951077f2b2ae3f18a1
mwierda <mwierda@uiowa.edu>
2019-11-13 19:11:16 -0600

    add movementList.csv and methods to post/get to it

--- movementLog.py
+++ movementLog.py
@@ -10,19 +10,37 @@ import csv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def downloadFile():
+def getKilotrainedCurrentWeek():
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json',scope)
-    # docName = "Competitor"
+    #docName = "Competitor"
     docID = "1-jmN0QV27PLUrqbsLp7M9FeYupNLE-Nu7UGNzJ0KWaI"
 
     client = gspread.authorize(credentials)
     competitorSpreadsheet = client.open_by_key(docID)
-
     currentWeekWS = competitorSpreadsheet.get_worksheet(0)
-    for rowVal in range(1,10):
-        print("current row: {}".format(rowVal))
-        (currentWeekWS.row_values(rowVal))
+    # # these should work
+    # valA1 = currentWeekWS.acell("B2").value
+    # row1 = currentWeekWS.row_values(1)
+    # print(valA1)
+    # print(row1)
+    # # this stopped working for some reason; idk why
+    # for row in range(1,10):
+    #     print(currentWeekWS.row_values(row))
+    return currentWeekWS
+
+def saveMovementToCSV(movement):
+    with open("movementList.csv", 'a', newline='') as movementRowsCSV:
+        csvWriter = csv.writer(movementListCSV)
+        csvWriter.writerow([movement])
+
+def getCurrentMovementList():
+    movementList = []
+    with open("movementList.csv", 'r', newline='') as movementRowsCSV:
+        csvReader = csv.reader(movementRowsCSV)
+        for row in csvReader:
+            movementList += row
+    return movementList
 
 class Movement:
     def __init__(self, name, sets = None, reps = None):
@@ -34,11 +52,13 @@ class Movement:
         return self.name # return string name of Movement
 
 def main():
-    print('wtf')
     backSquat = Movement('Back Squat')
     print(backSquat.getName())
-    #print(lift)
-    downloadFile()
+    currWeek = getKilotrainedCurrentWeek()
+    # print(currWeek.acell("B2").value) # works!
+    #saveMovementToCSV("deadlift")
+    movementList = getCurrentMovementList()
+    print(movementList)
 
 if __name__=='__main__':
     main()
